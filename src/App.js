6ac7351bd2bcd03717be52109fd759ef4c33cee4
cesaranibaljimenez <c_jimenez_guerra@hotmail.com>
2023-11-21 21:57:37 -0500

    Se completo la función onSbmit con las condicionales necesarias y se gregó la función isValidEmail

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import { useFormik } from 'formik';
 
 function App() {
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
   // TODO: add a const called formik assigned to useFormik()
 
   const formik = useFormik({
@@ -10,23 +13,37 @@ function App() {
       email: '',
       password: ''
     },
+
     onSubmit: values =>{
-      console.log('form:', values);
+      if(!values.email || !values.password){
+        alert('Field required');
+      } else if(!isValidEmail(values.email)){
+        alert('Username should be an email');
+      }else{
+        alert('Login Successful');
+      }
+        
     },
+      
+
     validate: values => {
      let errors = {};
-     if(!values.email) errors.email = 'Required'
-     if(!values.password) errors.password = 'Required'
+     if(!values.email) errors.email = 'Field required'
+     if(!values.password) errors.password = 'Field required'
      return errors;
     }
   });  
+
+    const isValidEmail = (email, regex) => {
+      return emailRegex.test(email);
+    };
 
   return (
     <div>
       
       <form onSubmit={formik.handleSubmit}>
       <div>Email</div>
-      <input name="emailField" type="text" onChange={formik.handleChange} value={formik.values.email}/>
+      <input name="email" type="text" onChange={formik.handleChange} value={formik.values.email}/>
       {formik.errors.email ? <div style={{color:'red'}}>{formik.errors.email}</div>: null}
       <div>Password</div>
       <input name="password" type="password" onChange={formik.handleChange} value={formik.values.password}/>
